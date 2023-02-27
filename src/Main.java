054c7f8e221c1b95f0afaedd635fb2f6f439bbb4
Quin Adams <quinadams@quins-air.lan>
2023-02-26 21:45:30 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        //Creating the rows needed
+        final int ROW = 5;
+        //Creating nested for loop
+  for (int rows = 1; rows <= ROW; rows++)
+  {
+      for (int col = 1; col <= 5; col++)
+      {
+          System.out.print("*");
+          {
+              System.out.println("");
+          }
+      }
+
+      }
+  }
+
+    }
